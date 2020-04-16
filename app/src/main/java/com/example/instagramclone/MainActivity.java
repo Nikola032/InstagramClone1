ffca9b172e5d4f7647be1c8fab4e0440776732d0
Nikola032 <Dzoni_032@yahoo.com>
2020-04-16 13:35:36 -0500

    wsign up and log in Pars

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -93,6 +94,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpLogInActivity.class);
+                startActivity(intent);
 
             }
         });
