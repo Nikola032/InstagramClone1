206ac5c1f6d763f39dace3de81d33513376dcec2
Nikola032 <Dzoni_032@yahoo.com>
2020-04-16 00:12:07 -0500

    Parse Server, Parse Object, External Library

---
+++ app/src/main/java/com/example/instagramclone/App.java
@@ -0,0 +1,23 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("7NwVLia3TK4hVNLD7X3R4v93wt2pLwAH3mUixddO")
+                // if defined
+                .clientKey("0OrKhjq1j3RrWjwVx5YbpZ8P39bHcaE9k4jvZr4k")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
+
+    }
+
+
+}
